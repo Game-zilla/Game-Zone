59eb282c4f8e89209960b8fba94f0bd0a35fab18
hardikk2002 <58857069+hardikk2002@users.noreply.github.com>
2020-03-14 11:28:46 +0530

    Update kbc.cpp

--- kbc.cpp
+++ kbc.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    char nameOfUser[50],inputValue[11];   // name and ch
-    int var=0;   //x
+    char nameOfUser[50],inputValue[11];   // variables name of user and input variable
+    int var=0;      // to the arithrmatics
    
     do
     {
@@ -19,8 +19,8 @@ int main()
         
         cout<<"\n\n Enter your Name: ";
         cin>>nameOfUser;
-        cout<<"\n Enter answer in Form of 'a','b' and'c'only.";
-        cout<<"\n Q1.What is called as ' THE HOLY LAND'?";
+        cout<<"\n Enter answer in Form of 'a','b' and'c'only.";     // Instructions For user
+        cout<<"\n Q1.What is called as ' THE HOLY LAND'?";       // first Question
         cout<<"\n a.Jerusalem";
         cout<<"\n b.Mathura";
         cout<<"\n c.Mecca";
@@ -31,12 +31,12 @@ int main()
             cout<<"\n Good Job.";
             cout<<"\n Your score is "<<var;
         }
-        else
+        else                                       // condition if answer is wrong
         {
             cout<<"\n Sorry wrong answer.";
-            goto abc;
+            goto abc;                              // goto calling statement
         }
-        cout<<"\n Q2.What is called as ' THE ROOF OF THE WORLD'?";
+        cout<<"\n Q2.What is called as ' THE ROOF OF THE WORLD'?";    // 2nd
         cout<<"\n a.Nepal";
         cout<<"\n b.Rome";
         cout<<"\n c.Tibet";
@@ -50,7 +50,7 @@ int main()
         else
         {
             cout<<"\n Sorry wrong answer.";
-            goto abc;
+            goto abc;                         // goto calling statement
         }
         cout<<"\n Q3.What is called as ' THE LAND OF RISING SUN'?";
         cout<<"\n a.Chicago";
@@ -66,7 +66,7 @@ int main()
         else
         {
             cout<<"\nSorry wrong answer.";
-            goto abc;
+            goto abc;                       // goto calling statement
         }
         cout<<"\n Q4.What is called as ' THE GIFT OF NILE'?";
         cout<<"\n a.Chicago";
@@ -82,7 +82,7 @@ int main()
         else
         {
             cout<<"Sorry wrong answer.";
-            goto abc;
+            goto abc;                       // goto calling statement
         }
         cout<<"\n Q5.What is called as ' THE LAND OF MIDNIGHT SUN'?";
         cout<<"\n a.Norway";
@@ -98,7 +98,7 @@ int main()
         else
         {
             cout<<"\n Sorry wrong answer.";
-            goto abc;
+            goto abc;                        // goto calling statement
         }
         cout<<"\n Q6.What is called as ' THE LAND OF THUNDERBOLT'?";
         cout<<"\n a.Bhutan";
@@ -114,7 +114,7 @@ int main()
         else
         {
             cout<<"\n Sorry wrong answer.";
-            goto abc;
+            goto abc;                 // goto calling statement
         }
         cout<<"\n Q7.What is called as ' THE WINDY CITY?";
         cout<<"\n a.Jerusalem";
@@ -130,7 +130,7 @@ int main()
         else
         {
             cout<<"\n Sorry wrong answer.";
-            goto abc;
+            goto abc;                  // goto calling statement
         }
         cout<<"\n Q8.What is called as ' THE LAND OF WHITE ELEPHANTS'?";
         cout<<"\n a.Bangladesh";
@@ -146,7 +146,7 @@ int main()
         else
         {
             cout<<"\n Sorry wrong answer.";
-            goto abc;
+            goto abc;                       // goto calling statement
         }
         cout<<"\n Q9.What is called as ' THE CITY OF SEVEN HILLS'?";
         cout<<"\n a.Rome";
@@ -162,7 +162,7 @@ int main()
         else
         {
             cout<<"\n Sorry wrong answer";
-            goto abc;
+            goto abc;                      // goto calling statement
         }
         cout<<"\n Q10.What is called as ' THE DARK CONTIENENT'?";
         cout<<"\n a.Asia";
@@ -178,22 +178,22 @@ int main()
         else
         {
             cout<<"\n Sorry wrong answer.";
-            goto abc;
+            goto abc;           // goto calling statement
         }
-        abc:
+        abc:                   // statement if goto is called
         if(var==100)
         {
-            cout<<"\n No cheating...... "<<nameOfUser<<" you have done this earlier also.";
+            cout<<"\n No cheating...... "<<nameOfUser<<" You have done this earlier also.";
             break;
         }
         if(var==90)
         {
-            cout<<"\n "<<nameOfUser<<" you are extremely intelligent.";
+            cout<<"\n "<<nameOfUser<<" You are extremely intelligent.";
             cout<<"\n Your Score is 90";
         }
         if(var==80)
         {
-            cout<<"\n "<<nameOfUser<<" you are intelligent.";
+            cout<<"\n "<<nameOfUser<<" You are intelligent.";
             cout<<"\n Your Score is 80";
         }
         if(50==var||var==70||var==60)
