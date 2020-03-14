bd89c087e639d9044176f4d92ab839146e2336f3
gerald-of-revia <58028567+majesty-c@users.noreply.github.com>
2020-03-14 12:52:22 +0530

    noob

--- kbc.cpp
+++ kbc.cpp
@@ -1,31 +1,29 @@
-#include<iostream>  
+#include<iostream>
 using namespace std;
+void interface()
+{
+cout<<"________________________________________________________________________\n";
+cout<<"|                       mai hoo amitabha bacchan                        |\n";
+cout<<"|                  Swagat hai aapka Kaun Banega Crorepati mein…         |\n";
+cout<<"|                    Chaliye is khel ko aage badhaya jaaye              |\n";
+cout<<"|_______________________________________________________________________|\n";
+}
 int main()
 {
     char nameOfUser[50],inputValue[11];   // variables name of user and input variable
     int var=0;      // to the arithrmatics
-   
-    do
+    interface();
+     do
     {
-        cout<<"                                              ___________________________________________________________    \n";
-        cout<<"                                        |                                                           |   \n";
-        cout<<"                                             |                                                           |   \n";
-        cout<<"                                         |                                                           |   \n";
-        cout<<"                                             |          W E L C O M E   T O   K B C                      |   \n";
-        cout<<"                                             |                                                           |   \n";
-        cout<<"                                             |                                                           |   \n";
-        cout<<"                                             |                                                           |   \n";
-        cout<<"                                             |___________________________________________________________|   \n";
-        
-        cout<<"\n\n Enter your Name: ";
+        cout<<"\n\n sriman apna naam bataye:";
         cin>>nameOfUser;
         cout<<"\n Enter answer in Form of 'a','b' and'c'only.";    // Instructions For user
         cout<<endl;
         cout<<endl;
-        cout<<"\n Q1.Who is the father of 'C' language ?";       // first Question
-        cout<<"\n a.";
-        cout<<"\n b.Mathura";
-        cout<<"\n c.Mecca";
+        cout<<"\n Q1.Who is the father of 'C' language ?";      // first Question
+        cout<<"\n a.Dennis Ritchie";
+        cout<<"\n b.Bjarne Stroustrup";
+        cout<<"\n c.Linus Torvalds";
         cin>>inputValue[1];
         if(inputValue[1]=='a')
         {
@@ -38,10 +36,10 @@ int main()
             cout<<"\n Sorry wrong answer.";
             goto abc;                              // goto calling statement
         }
-        cout<<"\n Q2.What is called as ' THE ROOF OF THE WORLD'?";    // 2nd
-        cout<<"\n a.Nepal";
-        cout<<"\n b.Rome";
-        cout<<"\n c.Tibet";
+        cout<<"\n Q2.Which of the following is the correct syntax of including a user defined header files in C++?";    // 2nd
+        cout<<"\n a.#include <userdefined.h>";
+        cout<<"\n b.#include <userdefined>";
+        cout<<"\n c.#include “userdefined";
         cin>>inputValue[2];
         if(inputValue[2]=='c')
         {
@@ -50,14 +48,14 @@ int main()
             cout<<"\n Your score is "<<var;
         }
         else
-        {
+      
             cout<<"\n Sorry wrong answer.";
             goto abc;                         // goto calling statement
-        }
-        cout<<"\n Q3.What is called as ' THE LAND OF RISING SUN'?";
-        cout<<"\n a.Chicago";
-        cout<<"\n b.Japan";
-        cout<<"\n c.Tibet";
+        
+        cout<<"\n Q3. Which of the following is a correct identifier in C++?";                  // 3rd
+        cout<<"\n a.7var_name";
+        cout<<"\n b.VAR_1234";
+        cout<<"\n c.$var_name";
         cin>>inputValue[3];
         if(inputValue[3]=='b')
         {
@@ -70,10 +68,10 @@ int main()
             cout<<"\nSorry wrong answer.";
             goto abc;                       // goto calling statement
         }
-        cout<<"\n Q4.What is called as ' THE GIFT OF NILE'?";
-        cout<<"\n a.Chicago";
-        cout<<"\n b.Egypt";
-        cout<<"\n c.Africa";
+        cout<<"\n Q4.Which of the following is called address operator?";                          //4 th  
+        cout<<"\n a.*";
+        cout<<"\n b. &";
+        cout<<"\n c. _";
         cin>>inputValue[4];
         if(inputValue[4]=='b')
         {
@@ -86,10 +84,10 @@ int main()
             cout<<"Sorry wrong answer.";
             goto abc;                       // goto calling statement
         }
-        cout<<"\n Q5.What is called as ' THE LAND OF MIDNIGHT SUN'?";
-        cout<<"\n a.Norway";
-        cout<<"\n b.Japan";
-        cout<<"\n c.Australia";
+        cout<<"\n Q5. Which of the following is used for comments in C++?";                        //5 th
+        cout<<"\n a.both // comment or /* comment */";
+        cout<<"\n b./* comment */";
+        cout<<"\n c.// comment */";
         cin>>inputValue[5];
         if(inputValue[5]=='a')
         {
@@ -102,10 +100,10 @@ int main()
             cout<<"\n Sorry wrong answer.";
             goto abc;                        // goto calling statement
         }
-        cout<<"\n Q6.What is called as ' THE LAND OF THUNDERBOLT'?";
-        cout<<"\n a.Bhutan";
-        cout<<"\n b.Canada";
-        cout<<"\n c.Arab";
+        cout<<"\n Q6.What are the actual parameters in C++?";                       //6th
+        cout<<"\n a.Parameters with which functions are called";
+        cout<<"\n b.Parameters which are used in the definition of a function";
+        cout<<"\n c.Variables other than passed parameters in a function";
         cin>>inputValue[6];
         if(inputValue[6]=='a')
         {
@@ -118,10 +116,10 @@ int main()
             cout<<"\n Sorry wrong answer.";
             goto abc;                 // goto calling statement
         }
-        cout<<"\n Q7.What is called as ' THE WINDY CITY?";
-        cout<<"\n a.Jerusalem";
-        cout<<"\n b.Japan";
-        cout<<"\n c.Chicago";
+        cout<<"\n Q7.What are the formal parameters in C++?";                          //7th
+        cout<<"\n a. Parameters with which functions are called";
+        cout<<"\n b. Variables other than passed parameters in a function";
+        cout<<"\n c.Parameters which are used in the definition of the function";
         cin>>inputValue[7];
         if(inputValue[7]=='c')
         {
@@ -134,10 +132,10 @@ int main()
             cout<<"\n Sorry wrong answer.";
             goto abc;                  // goto calling statement
         }
-        cout<<"\n Q8.What is called as ' THE LAND OF WHITE ELEPHANTS'?";
-        cout<<"\n a.Bangladesh";
-        cout<<"\n b.Thailand";
-        cout<<"\n c.India";
+        cout<<"\n Q8. Which function is used to read a single character from the console in C++?";                //8th
+        cout<<"\n a.getline(ch)";
+        cout<<"\n b.cin.get(ch)";
+        cout<<"\n c.read(ch)";
         cin>>inputValue[8];
         if(inputValue[8]=='b')
         {
@@ -150,10 +148,10 @@ int main()
             cout<<"\n Sorry wrong answer.";
             goto abc;                       // goto calling statement
         }
-        cout<<"\n Q9.What is called as ' THE CITY OF SEVEN HILLS'?";
-        cout<<"\n a.Rome";
-        cout<<"\n b.Nilgiri Hills";
-        cout<<"\n c.Tibet";
+        cout<<"\n Q9.Which function is used to write a single character to console in C++?";                         //9th
+        cout<<"\n a.cout.put(ch)";
+        cout<<"\n b.cout.putline(ch)";
+        cout<<"\n c.write(ch)";
         cin>>inputValue[9];
         if(inputValue[9]=='a')
         {
@@ -166,10 +164,10 @@ int main()
             cout<<"\n Sorry wrong answer";
             goto abc;                      // goto calling statement
         }
-        cout<<"\n Q10.What is called as ' THE DARK CONTIENENT'?";
-        cout<<"\n a.Asia";
-        cout<<"\n b.Australia";
-        cout<<"\n c.Africa";
+        cout<<"\n Q10.What are the escape sequences?";                          //10th
+        cout<<"\n a.Set of characters that are used in the name of the main function of the program";
+        cout<<"\n b.Set of characters that whose use are avoided in C++ programs";
+        cout<<"\n c.Set of characters that convey special meaning in a program";
         cin>>inputValue[10];
         if(inputValue[10]=='c')
         {
